fa0e0ce0b25520821b4aa69dedcac83fd0dde953
Hiep <danghiep0122@gmail.com>
2022-03-19 14:42:30 +0700

    add 2 file to staging area

---
+++ main.js
@@ -0,0 +1,15 @@
+// function cong(a, b) {
+//     return a + b;
+// }
+
+// var result = cong(2, 8);
+// console.log(result);
+
+function getContentLength(content) {
+    return content.length;
+}
+
+
+// Mở tab Console để xem kết quả trực quan
+console.log(getContentLength('JavaScript'));
+console.log(getContentLength('Hello World'));
