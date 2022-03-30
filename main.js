db36f8a25dbd01a04c9197b9f32ab08676ae7ab4
Hiep <danghiep0122@gmail.com>
2022-03-30 20:34:25 +0700

    30/03

--- main.js
+++ main.js
@@ -38,13 +38,143 @@
 
 // console.log(result); // Expected: "Honda - Mazda - Mercedes"
 
-function getLastElement(array) { 
-    return array[array.length - 1] 
-}
+// function getLastElement(array) { 
+//     return array[array.length - 1] 
+// }
 
-// Ví dụ sử dụng
-var animals = ['Monkey', 'Tiger', 'Elephant','Lion', 'Fox', 'Bird'];
-var result = getLastElement(animals);
+// // Ví dụ sử dụng
+// var animals = ['Monkey', 'Tiger', 'Elephant','Lion', 'Fox', 'Bird'];
+// var result = getLastElement(animals);
 
-console.log(result); // Expected: "Elephant"
-console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
+// console.log(result); // Expected: "Elephant"
+// console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
+// let date = new Date();
+// console.log(date.getDate())
+
+// let random = Math.floor(Math.random() * 100);
+
+// if (random < 10) {
+//     console.log('chuc mung thanh nien 🤑🤑🤑🤑');
+// } else {
+//     console.log('Tach roi nhe 🤡🤡🤡🤡');
+// }
+
+// const number = [12, 43, 65, 32, 96];
+// console.log(number [2]);
+
+// console.log(typeof (21, 54, 65, 5, 32, 65, 62));
+
+// const unChek = 20 / 'a';
+// console.log(isNaN(unChek));
+
+// let date = Math.floor(Math.random() * 10 );
+
+// if (date === 2) {
+//     console.log('Today is Monday 🍟');
+// } else if (date === 3) {
+//     console.log('Today is Tuesday 🍚');
+// } else if (date === 4) {
+//     console.log('Today is Wednesday 🥪')
+// } else if (date === 5) {
+//     console.log('Today is Thursday 🥓');
+// } else if (date === 6) {
+//     console.log('Today is Friday 🍤')
+// } else if (date === 7) {
+//     console.log('Today is Saturday 🍚');
+// } else if (date === 8) {
+//     console.log('Today is Sunday 🥪');
+// } else {
+//     console.log('Out of date 🥐')
+// }
+
+// const date = Math.floor(Math.random() * 10);
+// switch(date) {
+//     case 2:
+//         console.log('Today is Monday 🍟');
+//         break;
+//     case 3:
+//         console.log('Today is Tuesday 🍚');
+//         break;
+//     case 4:
+//         console.log('Today is Wednesday 🥪');
+//         break;
+//     case 5:
+//         console.log('Today is Thursday 🥓');
+//         break;
+//     case 6:
+//         console.log('Today is Friday 🍤');
+//         break;
+//     case 7:
+//         console.log('Today is Saturday 🍚');
+//         break;
+//     case 8:
+//         console.log('Today is Sunday 🥪');
+//         break;
+//     default:
+//         console.log('Out of date 🥐');
+// }
+
+// const course = {
+//     name: 'Python',
+//     coin: 25 0
+// }
+
+// if (course.coin > 0) {
+//     console.log(`${course.coin} Coins`);
+// } else {
+//     console.log('Free Course');
+// }
+
+// let result = course.coin > 0 ? `${course.coin} Coins` : 'Free Course';
+//     console.log(result);
+
+// const dayWeek = Math.floor(Math.random() * 10);
+
+// let weekCon = dayWeek < 7 ? 'It\' a weekday 👔' : 'Not define 🎇🎞🏉🏈🎮';
+// console.log(weekCon);
+
+//Loop
+
+// const myArray = [
+//     '🐶',
+//     '🐷',
+//     '🐱',
+//     '🐃',
+//     '🐵',
+//     '🐯',
+//     '🐍',
+//     '🦁',
+//     '🐣',
+//     '🦆',
+//     '🐘',
+//     '🐿',
+//     '🐟',
+//     '🦈',
+//     '🐺',
+//     '🦊',
+//     '🐀',
+// ]
+
+// const myString = 'Responsibility'
+
+// const myObject = {
+//     game: 'Pes',
+//     type: '⚽',
+//     time: 2022,
+//     shit: true,
+// }
+
+// const arrayLength = myArray.length
+
+// for (var i = 0; i < myArray.length; i++) {
+//     console.log (`you're ${myArray[i]}`);
+// }
+// for (var key in myArray) {
+    // console.log (`you're ${myArray[key]}`);
+// }
+
+// console.log(Object.keys(myObject))
+
+// for (var key of Object.values(myObject)) {
+//     console.log(key);
+// }
